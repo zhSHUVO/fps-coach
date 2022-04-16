6c750da6c1719275430ab2b08ab5601da7a073bc
zhSHUVO <zhshuvo99@gmail.com>
2022-04-17 01:31:28 +0600

    nav design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
     return (
-        <div className="App">
-            <h1>FPS Couch</h1>
+        <div>
+            <Header></Header>
         </div>
     );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="home">FPS Coach</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="checkout">Checkout</Nav.Link>
+                            <Nav.Link href="blogs" activeCla>
+                                Blogs
+                            </Nav.Link>
+                            <Nav.Link href="about">About</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
