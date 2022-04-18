bc50e829754884c35112bfeec7d89e0469eee1d8
zhSHUVO <zhshuvo99@gmail.com>
2022-04-18 15:56:20 +0600

    video section added in the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,22 @@ const Home = () => {
             <div className="banner mt-5 mb-5">
                 <img className="w-100" src={banner} alt="" />
             </div>
+
             <Lessons></Lessons>
+
+            <div className="mt-5">
+                <h1 className="text-center">Featured Video</h1>
+                <iframe
+                    className=" d-flex mx-auto"
+                    width="960"
+                    height="540"
+                    src="https://www.youtube.com/embed/spTutRGM6FQ"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                ></iframe>
+            </div>
 
             <footer className="d-flex flex-column align-items-center pt-3 pb-3 mt-5 bg-dark text-light">
                 <h2>FPS COACH</h2>
--- src/components/Lessons/Lessons.js
+++ src/components/Lessons/Lessons.js
@@ -14,29 +14,33 @@ const Lessons = (props) => {
     return (
         <div>
             <h1 className="text-center">My Services</h1>
-            <p className="text-center w-75 mx-auto">I'll help you learn the basics, tips and tricks. Will give you some practice routines for aim, sensitivity, movements etc. I'll help you get over your weaknesses.</p>
+            <p className="text-center w-75 mx-auto">
+                I'll help you learn the basics, tips and tricks. Will give you
+                some practice routines for aim, sensitivity, movements etc. I'll
+                help you get over your weaknesses.
+            </p>
             <div className="container">
-            <div className="row w-75 mx-auto">
-                {lessons.map((lesson) => (
-                    <div className="col" key={lesson.id}>
-                        <div className="card mt-3 p-2">
-                            <img
-                                className="mx-auto mb-3 mt-3"
-                                src={lesson.img}
-                                alt=""
-                            />
-                            <p>{lesson.name}</p>
-                            <p>{lesson.description}</p>
-                            <p>Trainig Price: {lesson.price}</p>
-                            <Link to="/checkout">
-                                <Button variant="danger" type="submit">
-                                    Hire Me
-                                </Button>
-                            </Link>
+                <div className="row w-75 mx-auto">
+                    {lessons.map((lesson) => (
+                        <div className="col" key={lesson.id}>
+                            <div className="card mt-3 p-2">
+                                <img
+                                    className="mx-auto mb-3 mt-3"
+                                    src={lesson.img}
+                                    alt=""
+                                />
+                                <p>{lesson.name}</p>
+                                <p>{lesson.description}</p>
+                                <p>Trainig Price: {lesson.price}</p>
+                                <Link to="/checkout">
+                                    <Button variant="danger" type="submit">
+                                        Hire Me
+                                    </Button>
+                                </Link>
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
